2b2a6a5161d75f88b6190a86b00df7a6fa9d449e
Songuyen-06 <soncrt1234@gmail.com>
2025-01-13 21:38:46 +0700

    update ui home page

--- Project/appointment_scheduling_app_client/src/components/patient/Header.tsx
+++ Project/appointment_scheduling_app_client/src/components/patient/Header.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import Navbar from "./Navbar";
-import BannerList from "./BannerList";
 
 const Header = () => {
   return (
-    <div >
-      <Navbar />
-      <BannerList />
-      
-    </div>
+    <>
+      <div
+        className="min-h-screen mb-4 w-full bg-cover bg-center flex items-center overflow-hidden relative"
+        style={{ backgroundImage: "url('/background.jpeg')" }}
+        id="Header"
+      >
+         <Navbar />
+        <div className="container text-center mx-auto py-6 px-6 md:px-20 lg:px-32 text-white relative z-10">
+          <h2 className="text-4xl sm:text-5xl md:text-[72px] inline-grid max-w-3xl font-semibold pt-20">Schedule an appointment and view your results online</h2>
+          <h2 className=" text-xl text-cyan-500 sm:text-xl md:inline-grid max-w-4xl ">Now you can make an appointment before coming for a check-up and view your test results online quickly anytime, anywhere.</h2>
+          <div className="space-x-6 mt-16 ">
+            <a className="border  border-white px-8 py-3 rounded " href="#" >Booking appoiment</a>
+            <a className="bg-cyan-600   px-8 py-3 rounded " href="#">Contact us</a>
+          </div>
+        </div>
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      </div>
+     
+    </>
   );
 };
 
